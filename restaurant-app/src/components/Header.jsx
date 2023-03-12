4a16ee4ade2bd4a7768247e73d2fc3f62c61c87d
Behailu Weldeyohannes <bweldeyohannes@gmail.com>
2023-03-11 19:07:42 -0500

    meta tags has been added

---
+++ restaurant-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = ()=>{
+return(
+    <header>
+<img src="logo" alt="" />
+    </header>
+)
+}
+
+export default Header;
---
+++ restaurant-app/src/components/Main.jsx
@@ -0,0 +1,10 @@
+
+const Main = () =>{
+    return(
+        <main>
+            <h2>Restaurant</h2>
+        </main>
+    )
+}
+
+export default Main
