9dd7d2e3198a8243aed58a8fd38321ab2cfebf97
Behailu Weldeyohannes <bweldeyohannes@gmail.com>
2023-04-07 00:20:08 -0400

    React Router

---
+++ React-Router/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+export default function App() {
+  return <h1>Hello world!</h1>;
+}
+
+function About(){
+  return(
+    <h1>About</h1>
+  )
+}
+
+
---
+++ React-Router/src/main.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route  } from "react-router-dom";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <h1>Hello, React Router!</h1>;
+}
+
+function About() {
+  return <h1>About page goes here! 🎉</h1>;
+}
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
